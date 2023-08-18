69be1db16af1db64b37a42840b1686501cd75b3b
rohankumar390 <87806043+rohankumar390@users.noreply.github.com>
2023-08-18 07:19:34 +0530

    35:31

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useSocket } from '../context/SocketProvider'
 
 const Room = () => {
-  return (
-    <h1>Room</h1>
-  )
+    const socket = useSocket();
+    const [remoteSocketId, setRemoteSocketId] = useState(null);
+    const [myStream, setMyStream] = useState(null)
+    const handleUserJoined = useCallback(({ email, id }) => {
+        console.log(`Email ${email} joined room`);
+        setRemoteSocketId(true);
+    }, [])
+    useEffect(() => {
+        socket.on("user:joined", handleUserJoined);
+
+        return () => {
+            socket.off("user:joined", handleUserJoined)
+        }
+    }, [socket, handleUserJoined])
+
+    const handleCallUser = useCallback(async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        });
+        setMyStream(stream)
+    }, [])
+    return (
+        <>
+            <h1>Room</h1>
+            <h4>{remoteSocketId ? "Connected" : "No on in Room"}</h4>
+            {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}</>
+    )
 }
 
 export default Room
