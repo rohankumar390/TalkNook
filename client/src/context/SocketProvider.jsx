7c4e54b814630ce3b282b409ea6afc5b9fb0eb35
rohankumar390 <87806043+rohankumar390@users.noreply.github.com>
2023-08-28 00:08:24 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://talk-nook-a.vercel.app/"), []);
 
   return (
     <SocketContext.Provider value={socket}>
