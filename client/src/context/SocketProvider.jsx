45ec7cf89df46efabe93b4981d0e27ed84a9063e
rohankumar390 <87806043+rohankumar390@users.noreply.github.com>
2023-08-28 08:38:31 +0530

    Backend Changes

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -7,10 +7,9 @@ const SocketContext = createContext(null);
 export const useSocket = () => {
   const socket = useContext(SocketContext);
   return socket;
-};
-
+}
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://nook-fg3m.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
